4f0e8151dfe6453eddf2a3849d068934a89cb080
languedetesreves-alt <langue.de.tes.reves@gmail.com>
2026-06-25 19:04:12 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors({
+     origin: "https://languedetesreves.com",
+     methods: ["POST"],
+   }));
 app.use(express.json());
 
 const PORT = 10000;
